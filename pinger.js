fd4b003e1b3c8b8bfbda2fdebc606fde580f1f2b
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-31 21:04:44 +0300

    update pinger.js

---
+++ pinger.js
@@ -0,0 +1,25 @@
+var http = require('http'); 
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'mc-fouand-server.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                   
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 0.5 * 60 * 1000); // 30сек
+}
+
+startKeepAlive();
